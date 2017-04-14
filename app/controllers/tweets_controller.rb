52d5ad7421746e0c48ad26f33b367add6f708de4
Takuya Saito <t-saito@mesena-net.com>
2017-04-14 20:36:16 +0900

    rails g devise:views devise用のビューファイルの生成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,9 @@
 class TweetsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
-    @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
+    @tweets = Tweet.page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
@@ -16,4 +18,7 @@ class TweetsController < ApplicationController
     params.permit(:name, :image, :text)
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
