760d259b35cf9e7ed7afe1cd5dac84c9defa9f1a
Takuya Saito <t-saito@mesena-net.com>
2017-04-20 03:14:56 +0900

    users/showビューの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   def show
     # binding.pry
-    @nickname = current_user.nickname
+    user = User.find(params[:id])
+    @nickname = user.nickname
     # @nickname = User.find(current_user.id).nickname
     # @nickname = User.find_by(id: current_user.id).nickname
     # @nickname = User.find_by_id(id: current_user.id).nickname
     # @nickname = User.where(id: current_user.id).first.nickname
-    @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
+    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
 end
