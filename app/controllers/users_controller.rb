633142243b728260048262d2bc51730943bd6733
Takuya Saito <t-saito@mesena-net.com>
2017-04-19 15:47:44 +0900

    showアクションの編集 @nicknameと@tweetsの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def show
+    @nickname = current_user.nickname
+    # @nickname = User.find(current_user.id).nickname
+    # @nickname = User.find_by(id: current_user.id).nickname
+    # @nickname = User.find_by_id(id: current_user.id).nickname
+    # @nickname = User.where(id: current_user.id).first.nickname
+    @tweets = Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+  end
 end
