dfa9738b8a372392bf980b89cbaa22f8119993c2
Takuya Saito <t-saito@mesena-net.com>
2017-04-20 00:35:24 +0900

    リンクの書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   post  'tweets'      =>  'tweets#create'              #ツイート投稿機能
   delete  'tweets/:id'  => 'tweets#destroy'
   get   'tweets/:id/edit'  =>  'tweets#edit',  as: :edit_tweet
-  patch  'tweets/:id'  =>  'tweets#update'
+  patch  'tweets/:id'  =>  'tweets#update',  as: :tweet
   get   'users/:id'   =>  'users#show',    as: :user   #ユーザー詳細画面
 end
