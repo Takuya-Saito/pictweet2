eefce175742deac6498ca038b83c7f4333b89953
Takuya Saito <t-saito@mesena-net.com>
2017-04-19 16:11:17 +0900

    テンプレートの編集 ヘッダーにマイページへのリンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  'tweets#index'                       #ルートパスの指定
-  get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
-  get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
-  post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
-  get   'users/:id'   =>  'users#show'       #ユーザー詳細画面
+  root  'tweets#index'                                 #ルートパスの指定
+  get   'tweets'      =>  'tweets#index'               #ツイート一覧画面
+  get   'tweets/new'  =>  'tweets#new'                 #ツイート投稿画面
+  post  'tweets'      =>  'tweets#create'              #ツイート投稿機能
+  get   'users/:id'   =>  'users#show',    as: :user   #ユーザー詳細画面
 end
