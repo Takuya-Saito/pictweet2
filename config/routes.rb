ad0c5c71985a4e68641c4ed1ce8cb791dbe2a2db
Takuya Saito <t-saito@mesena-net.com>
2017-04-20 02:32:20 +0900

    ルーティングの書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root  'tweets#index'                                 #ルートパスの指定
-  get   'tweets'      =>  'tweets#index'               #ツイート一覧画面
-  get   'tweets/new'  =>  'tweets#new'                 #ツイート投稿画面
-  post  'tweets'      =>  'tweets#create'              #ツイート投稿機能
-  delete  'tweets/:id'  => 'tweets#destroy'
-  get   'tweets/:id/edit'  =>  'tweets#edit',  as: :edit_tweet
-  patch  'tweets/:id'  =>  'tweets#update',  as: :tweet
-  get   'tweets/:id'  =>  'tweets#show'
-  get   'users/:id'   =>  'users#show',    as: :user   #ユーザー詳細画面
+  root 'tweets#index'
+  resources :tweets                     #tweets_controllerに対してのresourcesメソッド
+  resources :users, only: [:show]       #users_controllerに対してのresourcesメソッド
 end
